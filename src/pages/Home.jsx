7b35241a9849eacee637605128fa2e4f11b04b25
William Neild <wneild5@gmail.com>
2019-03-31 15:47:57 +0100

    fix urls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,8 @@ export default class Home extends Component {
         <main>
           <h1>Home Page</h1>
           <Card title="Projects" body="Herease uses can findes alles foi good pojeees=fss and plays us good gamess" butt="Projects" icon="gamepad" buttLink="/projects"/>
-          <Card title="About Us" body="We is good company, pay uus doo stuff pls. we get good advertismentes. pls emplyyye us" butt="About" icon="users"/>
-          <Card title="Contact Us" body="hear u can msgeage usses and get usses to doos syah voek for good moneieees" butt="Contact" icon="at"/>
+          <Card title="About Us" body="We is good company, pay uus doo stuff pls. we get good advertismentes. pls emplyyye us" butt="About" icon="users" buttLink="/about"/>
+          <Card title="Contact Us" body="hear u can msgeage usses and get usses to doos syah voek for good moneieees" butt="Contact" icon="at" buttLink="/contact"/>
         </main>
         <Footer/>
       </>
