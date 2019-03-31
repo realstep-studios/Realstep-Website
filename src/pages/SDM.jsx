419a07b0c28551f76ea4a570a65542badef87821
ZackisKawaii <46940379+ZackisKawaii@users.noreply.github.com>
2019-03-31 17:27:03 +0100

    Update to TapZ

--- src/pages/SDM.jsx
+++ src/pages/SDM.jsx
@@ -13,9 +13,9 @@ export default class SDM extends Component {
           <h1>Seneca Dark Mode Project</h1>
           <Card title="How to use" url="https://github.com/wsngamerz/Seneca-dark-mode" icon="graduation-cap">
             <p>
-              This App is currently under developement and will later be made more user friendly.
-              In order to use it for now, copy the following line of code into the inspect element of Seneca
-              
+              This app is currently under developement and will later be made more user friendly.
+              In order to use it for now, copy the following line of code into the inspect element of Seneca.
+            <br/>
               &lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;https://wsngamerz.github.io/Seneca-dark-mode/seneca.dark.css&quot; /&gt;
             </p>
           </Card> 
--- src/pages/TapZ.jsx
+++ src/pages/TapZ.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Navigation from "../components/navigation/Navigation"
 import Footer from "../components/footer/Footer"
+import Card from "../components/card/Card"
 
 export default class TapZ extends Component {
   render() {
@@ -10,6 +11,19 @@ export default class TapZ extends Component {
         <Navigation/>
         <main>
           <h1>TapZ Incremental Project</h1>
+
+          <Card title="Game Basics" url="https://tapz.realstep.co.uk/" butt = "Join the fight!" icon="gamepad">
+            <p>
+              Blah blah blah kill zombs tyhey die go boom yey breins n mony
+            </p>
+          </Card>
+
+          <Card title="Latest update" url="https://github.com/wsngamerz/TapZ-Incremental" butt = "Learn more" icon="pen">
+            <p>
+              I haven't done anything on it in 7 years cos i am lazy
+            </p>
+          </Card> 
+
         </main>
         <Footer/>
       </>
