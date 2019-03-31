5ca9c57eef4d300025cd1704de305ef3425141b1
ZackisKawaii <46940379+ZackisKawaii@users.noreply.github.com>
2019-03-31 17:38:51 +0100

    Update Tapz.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Navigation from "../components/navigation/Navigation"
 import Footer from "../components/footer/Footer"
+import Card from "../components/card/Card"
 
 export default class About extends Component {
   render() {
@@ -10,6 +11,16 @@ export default class About extends Component {
         <Navigation/>
         <main>
           <h1>About Page</h1>
+
+          <Card title="Productive Owner" icon="user-tie">
+            <i><h4>William Neild</h4></i>
+            <p>You can be friends with him if u want.</p>
+          </Card>       
+
+          <Card title="Less Productive Owner" icon="smile-beam">
+            <i><h4>Zack Jagger</h4></i>
+            <p>He is a very cool man!</p>
+          </Card>   
         </main>
         <Footer/>
       </>
