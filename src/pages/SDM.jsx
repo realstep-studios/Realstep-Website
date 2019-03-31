7e3aa72c0bc2a14f3572f253bee54bbda7fa83aa
William Neild <wneild5@gmail.com>
2019-03-31 17:39:01 +0100

    added code styles and sdm syntax highlighting

--- src/pages/SDM.jsx
+++ src/pages/SDM.jsx
@@ -11,12 +11,14 @@ export default class SDM extends Component {
         <Navigation/>
         <main>
           <h1>Seneca Dark Mode Project</h1>
-          <Card title="How to use" url="https://github.com/wsngamerz/Seneca-dark-mode" icon="graduation-cap">
+          <Card title="How to use" url="https://github.com/wsngamerz/Seneca-dark-mode" icon="graduation-cap" butt="Learn More">
             <p>
               This app is currently under developement and will later be made more user friendly.
               In order to use it for now, copy the following line of code into the inspect element of Seneca.
-            <br/>
-              &lt;link rel=&quot;stylesheet&quot; type=&quot;text/css&quot; href=&quot;https://wsngamerz.github.io/Seneca-dark-mode/seneca.dark.css&quot; /&gt;
+              <br/>
+              <code>
+                <span className="tag">&lt;</span><span className="element">link</span> <span className="attribute">rel</span>=&quot;stylesheet&quot; <span className="attribute">type</span>=&quot;text/css&quot; <span className="attribute">href</span>=&quot;https://wsngamerz.github.io/Seneca-dark-mode/seneca.dark.css&quot; <span className="tag">/&gt;</span>
+              </code>
             </p>
           </Card> 
         </main>
